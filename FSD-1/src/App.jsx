3a36e6d14c427244b2f7607debba112da2e98ae3
Jaskaran Singh <jaskaran192005@gmail.com>
2026-01-19 10:13:15 +0530

    Added counter app and todo app

---
+++ FSD-1/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from "react";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div>
+      <h2>Counter SPA</h2>
+      <p>Count: {count}</p>
+      <button onClick={() => setCount(count + 1)}>+</button>
+      <button onClick={() => setCount(count - 1)}>-</button>
+    </div>
+  );
+}
+
+export default App;
---
+++ FSD-2/src/App.jsx
@@ -0,0 +1,30 @@
+import { useState } from "react";
+
+function App() {
+  const [task, setTask] = useState("");
+  const [tasks, setTasks] = useState([]);
+
+  const addTask = () => {
+    setTasks([...tasks, task]);
+    setTask("");
+  };
+
+  return (
+    <div>
+      <h2>To-Do SPA</h2>
+      <input
+        value={task}
+        onChange={(e) => setTask(e.target.value)}
+      />
+      <button onClick={addTask}>Add</button>
+
+      <ul>
+        {tasks.map((t, i) => (
+          <li key={i}>{t}</li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default App;
